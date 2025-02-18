2ca2a34a8051bc78df13ea5b32073bc13bc77615
Lipsarani-samal <lipsarni.samal2001@gmail.com>
2025-02-18 17:52:38 +0530

    Modify Dashboard

---
+++ src/pages/Dashboard/User.tsx
@@ -0,0 +1,13 @@
+import DefaultLayout from '../../layout/DefaultLayout';
+const User: React.FC = () => {
+    
+
+    return (
+      <DefaultLayout children={'Admin Panel Page'}>
+        
+      </DefaultLayout>
+    );
+  };
+  
+  export default User;
+  
