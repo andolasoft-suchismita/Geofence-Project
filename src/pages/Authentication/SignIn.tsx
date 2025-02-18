dad2675b069a9513bb674e859f6d98ecd7aa2238
andolasoft-soumya <andolasoft.user244@gmail.com>
2025-02-18 12:02:21 +0530

    modified breadcrumb

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -13,6 +13,13 @@ const SignIn: React.FC = () => {
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
+      <Breadcrumb pageName="Sign In" />
 
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="flex flex-wrap items-center">
